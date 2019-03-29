ca059aed538a1fc4f18db02216769e4e3e42a143
Anouk Borsboom <a.borsboom@youngcapital.nl>
2019-03-29 14:11:43 +0100

    Continuous updatability for date

--- src/components/event.js
+++ src/components/event.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import {Icon, Typography, Grid} from '@material-ui/core';
-import Edit from '@material-ui/icons/Create'
+import {Icon, Typography, Grid, TextField} from '@material-ui/core';
+import Delete from '@material-ui/icons/Delete'
 import PropTypes from 'prop-types';
 
 const Event = (props) => {
   const {title, icon, date, dateEnd} = props;
   return (
-    <Grid container spacing={16}>
+    <Grid container alignItems={"center"} spacing={16}>
       <Grid item xs={1}>
         {icon}
       </Grid>
       <Grid item xs={2}>
-      {dateEnd ? `${date} - ${dateEnd}` : date}
+      {dateEnd ? <form><TextField
+        id="date"
+        label="Date Start"
+        defaultValue={date}
+        type="date"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      /><TextField
+        id="date"
+        label="Date End"
+        defaultValue={dateEnd}
+        type="date"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      /></form> : <form><TextField
+        id="date"
+        label="Date"
+        defaultValue={date}
+        type="date"
+        InputLabelProps={{
+          shrink: true,
+        }}
+      /></form>}
       </Grid>
       <Grid item xs={8}>
       <Typography variant="overline">{title}</Typography>
       </Grid>
       <Grid item>
-      <Edit/>
+      <Delete/>
       </Grid>
     </Grid>
   )
--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import * as serviceWorker from './serviceWorker';
 
 import Event from './components/event';
 
-const data =[{date: "30-03-2019", title: "foo", icon: "testIcon"}, {date: "31-03-2019", dateEnd: "05-04-2019", title: "bar", icon:"carIcon"}]
+const data =[
+  {date: "2019-03-03", title: "foo", icon: "bookmark"},
+  {date: "2019-03-05", title: "test", icon: "build"},
+  {date: "2019-03-05", title: "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttest testtesttesttest testtesttesttest testtesttesttest testtesttesttest testtesttesttest", icon: "build"},
+  {date: "2019-03-08", dateEnd: "2019-08-08", title: "bar", icon:"class"},
+  {date: "2019-03-08", dateEnd: "2019-08-08", title: "bar", icon:"class"}
+]
 
 const events = (data) => (
   data.map((event) => {
-    return(<Event title={event.title} date={event.date} dateEnd={event.dateEnd || null} icon={event.icon}/>)
+    return(<Event title={event.title} date={event.date} dateEnd={event.dateEnd} icon={event.icon}/>)
   })
 );
 
